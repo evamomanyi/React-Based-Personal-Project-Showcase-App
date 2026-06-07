b12402e0b74b899f0a9d157bb9c2e7ae78c4544b
evamomanyi <evanyaks13@gmail.com>
2026-06-07 12:59:54 +0300

    Updated for github pages

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup.js",
+  base: "/React-Based-Personal-Project-Showcase-App/"
   },
 });
